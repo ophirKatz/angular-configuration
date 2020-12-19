9fc136a26216d0114ac5ba4d42d70d9f0e2cc96b
Ophir Katz <ophir8396@campus.technion.ac.il>
2020-12-19 20:59:18 +0200

    first commit with app configuration

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+
+import { APP_CONFIG } from 'needle-configuration';
+import { AppConfigService } from './configuration/app-config.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'needle';
+
+  constructor(@Inject(APP_CONFIG) public config: AppConfigService) {
+
+  }
 }
